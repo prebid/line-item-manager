ed3ff72231e4e4dd31015c96ddb9f32378fd9114
Doug Shore <dshore@jwplayer.com>
2020-10-30 18:30:28 +0000

    Additional CLI validation

--- line_item_manager/cli.py
+++ line_item_manager/cli.py
@@ -10,11 +10,6 @@ import yaml
 
 from .config import config
 
-def validate_client(ctx):
-    svc = config.client.GetService('NetworkService', version=config.app['googleads']['version'])
-    name = svc.getCurrentNetwork()['displayName']
-    if not name == config.cli['network_name']:
-        raise click.UsageError(f"Network name found \'{name}\' does not match the network name provided \'{config.cli['network_name']}\'.", ctx=ctx)
 
 click.option = partial(click.option, show_default=True)
 
@@ -24,8 +19,8 @@ def cli():
 
 @cli.command()
 @click.argument('configfile', type=click.Path(exists=True))
-@click.option('--network-code', required=True, type=int, help='GAM network code, must reconcile with the network name.')
-@click.option('--network-name', required=True, help='GAM network name, must reconcile with the network code.')
+@click.option('--network-code', type=int, help='GAM network code, must reconcile with the network name.')
+@click.option('--network-name', help='GAM network name, must reconcile with the network code.')
 @click.option('--private-key-file', '-k', required=True, default='gam_creds.json', type=click.Path(exists=True), help='Path to json GAM crendials file.')
 @click.option('--single-order', '-s', is_flag=True, help='Create a single set of orders instead of orders per bidder.')
 @click.option('--bidder-code', '-b', multiple=True, help='Bidder code, which may be used multiple times.')
@@ -40,9 +35,21 @@ def create(ctx, configfile, **kwargs):
         raise click.UsageError('You must use --single-order or provide at least one --bidder-code', ctx=ctx)
     if kwargs['single_order'] and kwargs['bidder_code']:
         raise click.UsageError('Use of --single-order and --bidder-code is ambiguous.', ctx=ctx)
+
+    # set config
     config.user_configfile = configfile
     config.cli = kwargs
-    validate_client(ctx)
+
+    # validate
+    if not config.network_code:
+        raise click.UsageError('You must provide a network code as an option or set in the config file', ctx=ctx)
+
+    if not config.network_name:
+        raise click.UsageError('You must provide a network name as an option or set in the config file', ctx=ctx)
+
+    name = config.client.GetService('NetworkService').getCurrentNetwork()['displayName']
+    if not name == config.network_name:
+        raise click.UsageError(f"Network name found \'{name}\' does not match the network name provided \'{config.network_name}\'", ctx=ctx)
     return 0
 
 @cli.command()
--- line_item_manager/config.py
+++ line_item_manager/config.py
@@ -34,13 +34,20 @@ class Config:
             self._user = yaml.safe_load(fp)
 
     @property
+    def network_code(self):
+        return self.cli['network_code'] or self.user.get('publisher', {}).get('network_code')
+
+    @property
+    def network_name(self):
+        return self.cli['network_name'] or self.user.get('publisher', {}).get('network_name')
+
+    @property
     def client(self):
         if self._client is None:
             with open(pkg_resources.resource_filename('line_item_manager',
                                                       'conf.d/googleads.yaml')) as fp:
                 _cfg = yaml.safe_load(fp)
-            _cfg['ad_manager']['network_code'] = self.cli['network_code'] or \
-              self.user['publisher']['network_code']
+            _cfg['ad_manager']['network_code'] = self.network_code
             _cfg['ad_manager']['path_to_private_key_file'] = self.cli['private_key_file']
             self._client = ad_manager.AdManagerClient.LoadFromString(yaml.dump(_cfg))
         return self._client
