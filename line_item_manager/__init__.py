33ddab3501d16cba89348d50e09778f4f268a138
Doug Shore <dshore@jwplayer.com>
2021-05-26 10:20:25 -0400

    release 0.2.3

--- line_item_manager/__init__.py
+++ line_item_manager/__init__.py
@@ -2,10 +2,10 @@
 
 __author__ = """the prebid contributors"""
 __email__ = 'info@prebid.org'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 # For an official release, use dev_version = ''
-dev_version = '4'
+dev_version = ''
 
 version = __version__
 if dev_version:
