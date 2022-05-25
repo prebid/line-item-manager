6648534fefa1943e5f7aa357be483de65022b527
Doug Shore <dshore@jwplayer.com>
2022-05-24 21:05:32 -0400

    Release 0.2.8

--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     url='https://github.com/prebid/line-item-manager',
-    version='0.2.7',
+    version='0.2.8',
     zip_safe=False,
 )
