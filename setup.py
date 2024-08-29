9981d6b9937734501593bf49db93a8e90a0e1412
Doug Shore <dshore@jwplayer.com>
2024-08-29 18:55:33 -0400

    Deprecate Python 3.7

--- setup.py
+++ setup.py
@@ -45,14 +45,13 @@ test_requirements = [
 setup(
     author="the prebid contributors",
     author_email='info@prebid.org',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
